c6deef767eef08536f7d590d3c2ddbb5e7fb9fe0
kakyoin <jo999dio@sina.com>
2015-01-16 11:43:13 +0800

    "first commit"

---
+++ robot/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from views import hello, page_404, current_datetime, hours_ahead
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'robot.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url('^hello/$', hello),
+    url('^404/$', page_404),
+    url('^time/$', current_datetime),
+    url(r'^time/plus/(\d{1,2})/$', hours_ahead),
+)
---
+++ robot/views.py
@@ -0,0 +1,32 @@
+import datetime
+from django.http import HttpResponse, Http404
+from django.shortcuts import render_to_response
+
+
+def hello(request):
+    return HttpResponse("Hello world")
+
+
+def page_404(request):
+    return render_to_response('error-404.html', {})
+
+
+# def current_datetime(request):
+#     now = datetime.datetime.now()
+#     html = "<html><body>It is now %s.</body></html>" % now
+#     return HttpResponse(html)
+
+
+def current_datetime(request):
+    now = datetime.datetime.now()
+    return render_to_response('current_datetime.html', {'current_date': now})
+
+
+def hours_ahead(request, offset):
+    try:
+        offset = int(offset)
+    except ValueError:
+        raise Http404()
+    dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
+    # html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
+    return render_to_response('hours_ahead.html', {'hour_offset': offset, 'next_time': dt})
