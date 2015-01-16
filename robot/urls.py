860c48916b80afa63b88e3cdeb1fdb46b1befaff
kakyoin <jo999dio@sina.com>
2015-01-16 15:32:08 +0800

    test2

--- robot/urls.py
+++ robot/urls.py
@@ -9,7 +9,7 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url('^hello/$', hello),
-    url("^406/$", page_404),
+    url("^407/$", page_404),
     url('^time/$', current_datetime),
     url(r'^time/plus/(\d{1,2})/$', hours_ahead),
 )
