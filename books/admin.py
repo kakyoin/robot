cfc16434df1eb3d2eb1532bd0b5147be0277d0ca
kakyoin <jo999dio@sina.com>
2015-01-19 17:57:42 +0800

    windows 407

--- books/admin.py
+++ books/admin.py
@@ -2,6 +2,15 @@ from django.contrib import admin
 
 from books.models import Publisher, Author, Book
 
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email')
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'publisher', 'publication_date')
+    list_filter = ('publication_date',)
+
 admin.site.register(Publisher)
-admin.site.register(Author)
-admin.site.register(Book)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Book, BookAdmin)
